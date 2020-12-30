9b7ab270f45ed94f15bda16d87989da55fed904e
Jean Herbert <73619022+jch1993-web@users.noreply.github.com>
2020-12-29 22:36:05 -0300

    Add files via upload

---
+++ ex012.js
@@ -0,0 +1,10 @@
+var hora = new Date();
+hora.getHours()
+console.log(`Olá, são exatamente ${hora.getHours()} horas`)
+if (hora <= 12) {
+    console.log('Bom dia!')
+} else if (hora <= 18) {
+    console.log('Boa tarde!')
+} else {
+    console.log('Boa noite!')
+}
